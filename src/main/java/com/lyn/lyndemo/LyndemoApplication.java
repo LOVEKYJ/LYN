ccd18edab62f7edfa39a0bdbc5eef1f681dfe748
KYJ&LYN <2411351195@qq.com>
2018-11-05 20:52:05 +0800

    first 代码

---
+++ src/main/java/com/lyn/lyndemo/LyndemoApplication.java
@@ -0,0 +1,12 @@
+package com.lyn.lyndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyndemoApplication.class, args);
+    }
+}
